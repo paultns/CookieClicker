f54bb83d46d9b87efdce069e75960aa6a3f2db05
Pablo Escobar <pabloescobar@keemail.me>
2018-06-06 11:35:11 +0300

    v1.0 Basic Cookie clicker

--- src/CookieClicker.java
+++ src/CookieClicker.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class CookieClicker {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
@@ -15,13 +17,15 @@ public class CookieClicker {
         //driver = new InternetExplorerDriver();
         driver.manage().window().maximize();
 
-        //driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         driver.get("http://orteil.dashnet.org/cookieclicker/");
         Thread.sleep(2000);
         while (true) {
-            if (isElementPresent(By.cssSelector("#div[class*='unlocked'"), driver))
-                driver.findElement(By.cssSelector("#div[class*='unlocked'")).click();
+            if (isElementPresent(By.cssSelector("div[class*='upgrade enabled']"), driver))
+                driver.findElement(By.cssSelector("div[class*='upgrade enabled']")).click();
+            else if (isElementPresent(By.cssSelector("div[class*='unlocked enabled']"), driver))
+                driver.findElement(By.cssSelector("div[class*='unlocked enabled']")).click();
             driver.findElement(By.cssSelector("#bigCookie")).click();
         }
     }
